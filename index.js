9479d285a797ba47a381ad7063ca632f7c75d2d1
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-05 12:16:45 -0600

    working on event bubbling

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ headingOne.style.color = 'red';
 // console.log(document.querySelector('.ingredients'));
 const ingredients = document.querySelectorAll('.ingredients');
 console.log(ingredients[0].className);
+
+const ingredientInput = document.getElementsById('ingredient-input');
+
+ingredientInput.addEventListener('keypress', function(event){
+    console.log(event)
+}) 
+
+document.getElementById('child').addEventListener('click', function(event) {
+    event.stopPropagation()
+    console.log('child button clicked')
+})
+
+function submit() {
+    console.log("Button clicked");
+}
+
+document.getElementById('form-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('form submitted')
+})
