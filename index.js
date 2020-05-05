49e10951aa43fe4231b7743084dd234e70eb4e5d
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-05 11:22:13 -0600

    practicing node properties

---
+++ index.js
@@ -0,0 +1,13 @@
+const headingOne = document.getElementById("heading-one");
+headingOne.innerText = 'Cinco de Mayo | Taco Tuesday Shopping List'
+headingOne.style.color = 'red';
+
+
+
+// console.log(document.querySelector('ul').innerHTML);
+
+
+// console.log(document.getElementsByTagName('li'));
+// console.log(document.querySelector('.ingredients'));
+const ingredients = document.querySelectorAll('.ingredients');
+console.log(ingredients[0].className);
